34d9f7038a4c0c481f17aa5dd46fe7a867ef6434
Marceli <marcel.piekalkiewicz@gmail.com>
2011-12-19 11:54:38 +0000

    Adding FizzBuzz.cs

---
+++ Algorithms/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Algorithms
+{
+    [TestFixture]
+    public class FizzBuzz
+    {
+        [Test]
+        public void DoFizzBuzz()
+        {
+            Enumerable.Range(1,100).ToList().ForEach(el=>Console.WriteLine(Fizz(el)));
+        }
+       string Fizz(int i)
+        {
+            if (i % 3==0 && i % 5==0)
+                return "FizzBuzz";
+            if (i % 3==0)
+                return "Fizz";
+            if (i % 5 == 0)
+                return "Buzz";
+            return i.ToString();
+        }
+    }
+}
