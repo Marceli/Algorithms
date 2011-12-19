1ab9c4ec508391c6eb635c938ea97ac4402e2daf
Marceli <marcel.piekalkiewicz@gmail.com>
2011-12-19 16:53:39 +0000

    new

---
+++ Algorithms/SumCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Algorithms
+{
+    [TestFixture]
+    public class SumCollection
+    {
+        [Test]
+        public void Test()
+        {
+            Enumerable.Range(1, 9).Sum();
+        }
+    }
+}
